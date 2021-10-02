b08bbcc3a50b4a48ec3d0cf6c998283d6d36aeba
StarkCross <alex_avilamedina@hotmail.com>
2021-10-02 12:49:56 -0500

    ActualizaFireBase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,11 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyAjI0blGZQ5khSw22GcgVW4xvxcPCdhw5Y",
-    authDomain: "usuariosapp-2bd3a.firebaseapp.com",
-    projectId: "usuariosapp-2bd3a",
-    storageBucket: "usuariosapp-2bd3a.appspot.com",
-    messagingSenderId: "888678793302",
-    appId: "1:888678793302:web:c0b2c2e6156b8739e03bf7"
+    apiKey: "AIzaSyCB5vQB1zpft8rZtoAKJiosVtuZ1Z98rhk",
+    authDomain: "usuariosappcsd.firebaseapp.com",
+    projectId: "usuariosappcsd",
+    storageBucket: "usuariosappcsd.appspot.com",
+    messagingSenderId: "64986634670",
+    appId: "1:64986634670:web:a50f79e3aa2087c5d0503f"
   }
 };
